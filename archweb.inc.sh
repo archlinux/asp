c96f1d6c510c1a737ed4549953a0d7acba63ac79
progandy <progandy@users.noreply.github.com>
2021-01-07 02:27:21 +0100

    Fix archweb url to follow new redirection
    ```www.archlinux.org``` now redirects to ```archlinux.org```.
    Reflect that in the the ```curl``` command and tell it to follow
    redirects from now on.

--- archweb.inc.sh
+++ archweb.inc.sh
@@ -1,7 +1,7 @@
 archweb_get_pkgbase() {
   local pkgbase
 
-  pkgbase=$(curl -Gs 'https://www.archlinux.org/packages/search/json/' --data-urlencode "q=$1" |
+  pkgbase=$(curl -LGs 'https://archlinux.org/packages/search/json/' --data-urlencode "q=$1" |
       jq -r --arg pkgname "$1" 'limit(1; .results[] | select(.pkgname == $pkgname).pkgbase)')
   [[ $pkgbase ]] || return
 
